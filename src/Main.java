2f3b2527729709fc1f160f8e8ccc3ae3532966d6
Anastasiia <anastasia994@yandex.ru>
2023-02-18 14:37:20 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int score = 50; // счет клиента
+        int added = 1100; // депозит / пополнение
+
+        int bonus; // объявление переменной для количества бонусов
+        if (added > 1000) {
+            bonus = added / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = score + added + bonus;
+        System.out.println("Ваш итоговый счет: " + balance + " руб.");
+        System.out.println("Ваш бонус: " + bonus + " руб.");
     }
 }
